f3ecb57f6ba2ba7feb870b9add87a361639c7e6a
StephenNGeronimo <geronimostephen0516@gmail.com>
2024-05-25 12:38:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,8 @@ const btnLike2 = document.getElementById("btnLike2")
 const countLikes1 = document.getElementById("countLikes1")
 const countLikes2 = document.getElementById("countLikes2")
 
-
 function hitLikes1() {
   let totalLikes1 = parseInt(countLikes1.value) + 1
   countLikes1.textContent = totalLikes1.toString()
 }
-function hitLikes2() {
-  let totalLikes2 = parseInt(countLikes2.value) + 1
-  countLikes2.textContent = totalLikes2.toString()
-}
-btnLike1.addEventListenner("click",hitLikes1)
-btnLike2.addEventListenner("click",hitLikes2)
+btnLike1.addEventListener("click",hitLikes1)
