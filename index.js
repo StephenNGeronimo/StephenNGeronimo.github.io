0f15eeae4dc685ad40337f4eb934b5115f6e45f7
StephenNGeronimo <geronimostephen0516@gmail.com>
2024-05-25 12:34:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const countLikes1 = document.getElementById("countLikes1")
 const countLikes2 = document.getElementById("countLikes2")
 
 
-function hitLikes1(){
+function hitLikes1() {
   let totalLikes1 = parseInt(countLikes1.value) + 1
   countLikes1.textContent = totalLikes1.toString()
 }
-function hitLikes2 (){
+function hitLikes2() {
   let totalLikes2 = parseInt(countLikes2.value) + 1
   countLikes2.textContent = totalLikes2.toString()
 }
