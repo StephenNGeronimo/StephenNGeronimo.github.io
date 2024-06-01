7d9e23d7e688ed0296fd5d60b3f84577866bb6ad
StephenNGeronimo <geronimostephen0516@gmail.com>
2024-06-01 16:25:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,4 +13,6 @@ function hitLikes2() {
   let totalLikes2 = parseInt(countLikes2.value) + 1
   countLikes2.textContent = totalLikes2.toString()
 }
-btnLike2.addEventListener("click",hitLikes1)
+btnLike2.addEventListener("click",hitLikes2)
+
+
