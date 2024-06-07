0620215361ea19c874fa063b4607d52493d05572
StephenNGeronimo <geronimostephen0516@gmail.com>
2024-06-07 22:44:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const btnLike1 = document.getElementById("btnLike1")
+const btndisLike1 = document.getElementById("btndisLike1")
 const btnLike2 = document.getElementById("btnLike2")
+const btndisLike2 = document.getElementById("btndisLike2")
 const countLikes1 = document.getElementById("countLikes1")
+const countDislikes1 = document.getElementById("countDislikes1")
 const countLikes2 = document.getElementById("countLikes2")
+const countDislikes2 = document.getElementById("countDislikes2")
 
 function hitLikes1() {
   let totalLikes1 = parseInt(countLikes1.value) + 1
@@ -14,5 +18,11 @@ function hitLikes2() {
   countLikes2.textContent = totalLikes2.toString()
 }
 btnLike2.addEventListener("click",hitLikes2)
+
+function hitLikes3() {
+  let totalLikes3 = parseInt(countDilikes1.value) + 1
+  countDislikes1.textContent = totalLikes3.toString()
+}
+btndisLike1.addEventListener("click",hitLikes1)
 
 
